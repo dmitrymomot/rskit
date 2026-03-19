5cd6e7fa10bc075e4a9fa0cc63f55e3ab2aabdfc
Dmytro Momot <mail@dmomot.com>
2026-03-19 15:21:46 +0700

    feat: add modo::Config aggregate type

--- src/modo_config.rs
+++ src/modo_config.rs
@@ -1,1 +1,10 @@
-// TODO: implement Config struct
+use serde::Deserialize;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Deserialize, Default)]
+#[serde(default)]
+pub struct Config {
+    // server field added in Task 12 when server::Config exists
+    pub database: crate::db::Config,
+    pub tracing: crate::tracing::Config,
+}
