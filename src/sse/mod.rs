c3f85da2246c0e3be26d1b7144af7af3dc23fd6a
Dmytro Momot <mail@dmomot.com>
2026-03-21 16:03:04 +0700

    feat(sse): add Sender for imperative event production

--- src/sse/mod.rs
+++ src/sse/mod.rs
@@ -2,8 +2,10 @@ mod broadcaster;
 mod config;
 mod event;
 mod last_event_id;
+mod sender;
 
 pub use broadcaster::{BroadcastStream, Broadcaster, LagPolicy, replay};
 pub use config::SseConfig;
 pub use event::Event;
 pub use last_event_id::LastEventId;
+pub use sender::Sender;
