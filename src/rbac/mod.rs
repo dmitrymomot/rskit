586b2ba7243f1c3035e183773fe45bd9c8b084fc
Dmytro Momot <mail@dmomot.com>
2026-03-23 13:46:22 +0700

    feat(rbac): add require_role and require_authenticated guard layers

--- src/rbac/mod.rs
+++ src/rbac/mod.rs
@@ -1,7 +1,9 @@
 mod extractor;
+mod guard;
 mod middleware;
 mod traits;
 
 pub use extractor::Role;
+pub use guard::{require_authenticated, require_role};
 pub use middleware::middleware;
 pub use traits::RoleExtractor;
