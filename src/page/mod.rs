b8a9587c91d1582d10d57b4f8a70040a0a27bc15
Dmytro Momot <mail@dmomot.com>
2026-03-27 20:26:01 +0700

    feat(page): add Paginate offset pagination builder

--- src/page/mod.rs
+++ src/page/mod.rs
@@ -1,10 +1,12 @@
 mod config;
+mod offset;
 mod request;
 mod response;
 mod value;
 
 pub use config::PaginationConfig;
+pub use offset::Paginate;
 pub use request::{CursorRequest, PageRequest};
 pub use response::{CursorPage, Page};
 
-pub(crate) use value::{build_args, IntoSqliteValue, SqliteValue};
+pub(crate) use value::{IntoSqliteValue, SqliteValue, build_args};
