317c89ae9814243c0a4337906d7efc0947e95e25
Dmytro Momot <mail@dmomot.com>
2026-03-27 20:22:44 +0700

    feat(page): add Page<T> and CursorPage<T> response types

--- src/page/mod.rs
+++ src/page/mod.rs
@@ -1,6 +1,8 @@
 mod config;
+mod response;
 mod value;
 
 pub use config::PaginationConfig;
+pub use response::{CursorPage, Page};
 
 pub(crate) use value::{build_args, IntoSqliteValue, SqliteValue};
